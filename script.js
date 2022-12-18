8efb0ed8fdb82961f61cf5d77ac614c7339655fe
thewakar <thewakar1@gmail.com>
2022-12-18 11:43:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,18 @@
-// api url
-const api_url =
-	"https://employeedetails.free.beeceptor.com/my/api/path";
-
-// Defining async function
-async function getapi(url) {
-	
-	// Storing response
-	const response = await fetch(url);
-	
-	// Storing data in form of JSON
-	var data = await response.json();
-	console.log(data);
-	if (response) {
-		hideloader();
-	}
-	show(data);
-}
-// Calling that async function
-getapi(api_url);
-
-// Function to hide the loader
-function hideloader() {
-	document.getElementById('loading').style.display = 'none';
-}
-// Function to define innerHTML for HTML table
-function show(data) {
-	let tab =
-		`<tr>
-		<th>Name</th>
-		<th>Office</th>
-		<th>Position</th>
-		<th>Salary</th>
-		</tr>`;
-	
-	// Loop to access all rows
-	for (let r of data.list) {
-		tab += `<tr>
-	<td>${r.name} </td>
-	<td>${r.office}</td>
-	<td>${r.position}</td>
-	<td>${r.salary}</td>		
-</tr>`;
-	}
-	// Setting innerHTML as tab variable
-	document.getElementById("employees").innerHTML = tab;
+function loadContent () {
+  // (A) FETCH "DUMMY.HTML"
+  fetch("https://script.google.com/macros/s/AKfycbw0N43-yCGvdihQs3JCM8OVDjUC9YqhQ9Le7Y-ctPhGNV9tOyuUpKzQkNouYaw--rpD6A/exec")
+ 
+  // (B) RETURN THE RESULT AS TEXT
+  .then((result) => {
+    if (result.status != 200) { throw new Error("Bad Server Response"); }
+    return result.text();
+  })
+ 
+  // (C) PUT LOADED CONTENT INTO <DIV>
+  .then((content) => {
+    document.getElementById("demoShow").innerHTML = content;
+  })
+ 
+  // (D) HANDLE ERRORS - OPTIONAL
+  .catch((error) => { console.log(error); });
 }
